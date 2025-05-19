15bdb939b504be4b9ae2ab5665b9cde252f84d55
LMBots <64574207+Bivash4567@users.noreply.github.com>
2025-05-19 22:36:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = os.environ.get("API_ID", "27140753") #Replace With your api id
-API_HASH = os.environ.get("API_HASH", "159ed5aea5a4e464be18a16ecd26e524") #Replace With your api hash
+API_ID = os.environ.get("API_ID", "21975327") #Replace With your api id
+API_HASH = os.environ.get("API_HASH", "129d55fdc9256056f9388827dcd4428c") #Replace With your api hash
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7783815475:AAHZg_zn6nPJLVPaN-jN1GPSEzsfCy2QWQ0") #Replace With your bot token
