373d6bdb0b02d9cc6cca173a30164bf78346640a
Harmanpreet Singh <146160902+ohiharman@users.noreply.github.com>
2025-06-28 22:04:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = os.environ.get("API_ID", "21975327") #Replace With your api id
-API_HASH = os.environ.get("API_HASH", "129d55fdc9256056f9388827dcd4428c") #Replace With your api hash
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7783815475:AAHZg_zn6nPJLVPaN-jN1GPSEzsfCy2QWQ0") #Replace With your bot token
+API_ID = os.environ.get("API_ID", "27680167") #Replace With your api id
+API_HASH = os.environ.get("API_HASH", "90b8bad42e6210d0e1e04a858e045f55") #Replace With your api hash
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7566505677:AAEPEbFNPh1dTwoLyEokf1pgx72Ilz_Jhzw") #Replace With your bot token
